29007ae0c4687c1a19030b2daa1940e00c9679fe
serge <125203998+serge740@users.noreply.github.com>
2026-03-11 00:05:55 +0300

    added

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -281,16 +281,16 @@ const Navbar: React.FC<NavbarProps> = ({ scrolled }) => {
                                 Contact
                             </NavLink>
                         </li>
-                        <li className="ml-2">
+                    </ul>
+
+                        
                             <Link
                                 to="/donate"
                                 className="px-6 py-2.5 bg-gradient-to-r from-green-600 to-green-700 text-white font-semibold rounded-lg hover:from-green-700 hover:to-green-800 transition-all duration-300 shadow-md hover:shadow-lg hover:-translate-y-0.5 no-underline inline-block"
                             >
                                 Donate Now
                             </Link>
-                        </li>
-                    </ul>
-
+                        
                     {/* Mobile Menu Button */}
                     <button
                         onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
